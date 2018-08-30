01eeded29695216b6e2f6575ee594916d94d4306
JOBOPTIONSINC\pdelosreyes <patrick@chefdelosreyes.com>
2018-08-30 11:56:02 -0700

    schema compares + sql cleanup

---
+++ Databases/Applications/Tables/ApplicantLocation.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ApplicantLocation] (
+    [id]            INT           IDENTITY (10000, 1) NOT NULL,
+    [ApplicantId]   INT           NOT NULL,
+    [ApplicationId] INT           NULL,
+    [Location]      VARCHAR (256) NOT NULL,
+    [Note]          VARCHAR (MAX) NULL,
+    [CreateUser]    VARCHAR (128) CONSTRAINT [DF_ApplicantLocation_CreateUser] DEFAULT ('suser_name') NOT NULL,
+    [CreateDate]    DATETIME2 (0) CONSTRAINT [DF_ApplicantLocation_CreateDate] DEFAULT (getdate()) NOT NULL,
+    [ModifyUser]    VARCHAR (128) CONSTRAINT [DF_ApplicantLocation_ModifyUser] DEFAULT ('suser_name') NOT NULL,
+    [ModifyDate]    DATETIME2 (0) CONSTRAINT [DF_ApplicantLocation_ModifyDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_ApplicantLocation] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_ApplicantLocation_Applicant] FOREIGN KEY ([ApplicantId]) REFERENCES [dbo].[Applicant] ([id]) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT [FK_ApplicantLocation_Application] FOREIGN KEY ([ApplicationId]) REFERENCES [dbo].[Application] ([id]) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
---
+++ Databases/Applications/Tables/ApplicantPosition.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ApplicantPosition] (
+    [id]            INT           IDENTITY (10000, 1) NOT NULL,
+    [ApplicantId]   INT           NOT NULL,
+    [ApplicationId] INT           NULL,
+    [Position]      VARCHAR (256) NOT NULL,
+    [Note]          VARCHAR (MAX) NULL,
+    [CreateUser]    VARCHAR (128) CONSTRAINT [DF_ApplicantPosition_CreateUser] DEFAULT ('suser_name') NOT NULL,
+    [CreateDate]    DATETIME2 (0) CONSTRAINT [DF_ApplicantPosition_CreateDate] DEFAULT (getdate()) NOT NULL,
+    [ModifyUser]    VARCHAR (128) CONSTRAINT [DF_ApplicantPosition_ModifyUser] DEFAULT ('suser_name') NOT NULL,
+    [ModifyDate]    DATETIME2 (0) CONSTRAINT [DF_ApplicantPosition_ModifyDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_ApplicantPosition] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_ApplicantPosition_Applicant] FOREIGN KEY ([ApplicantId]) REFERENCES [dbo].[Applicant] ([id]) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT [FK_ApplicantPosition_Application] FOREIGN KEY ([ApplicationId]) REFERENCES [dbo].[Application] ([id]) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
